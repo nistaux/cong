abc8658f33daf95fba79a71f2a634974f66dea8a
Nistaux <nistaux@gmail.com>
2022-11-29 18:18:18 -0500

    first commit

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2\SDL.h>
+#include <stdbool.h>
+#include <defs.h>
+
+int main(int argc, char **argv)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("Cong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+    bool running = true;
+    SDL_Event event;
+    while(running == true) {
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT) {
+                running  = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
