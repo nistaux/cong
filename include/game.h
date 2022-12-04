21b9f0249afde379329f916ea2e4bee913efc88b
Nistaux <nistaux@gmail.com>
2022-12-04 14:28:32 -0500

    reorg game header

--- include/game.h
+++ include/game.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <states.h>
 
-void tick();
 Game *get_game();
 void init_game();
 void end_game();
 void set_game_state(State state);
 void set_game_running(bool running);
+
+void tick();
